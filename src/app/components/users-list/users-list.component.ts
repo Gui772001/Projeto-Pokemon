9684e3840a2784aa525049735f56a3c78c6f829c
guilherme <gulhermegui772001@gmail.com>
2025-04-13 16:38:13 -0300

    alterações

--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { PokemonBasicInfo, PokemonComparador, PokemonDetails } from '../../interfaces/Pokemons/PokemonListResponse';
+import { PokemonBasicInfo, PokemonDetails } from '../../interfaces/Pokemons/PokemonListResponse';
 import { PageEvent } from '@angular/material/paginator';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
@@ -10,57 +10,86 @@ import { firstValueFrom } from 'rxjs';
   styleUrls: ['./users-list.component.scss']
 })
 export class UsersListComponent {
+
   constructor(private http: HttpClient) { }
-
-  selectedPokemons: PokemonDetails[] = [];
-
-
-isPokemonSelected(pokemon: PokemonDetails): boolean {
-  return this.selectedPokemons.some(p => p.name === pokemon.name);
-  }
-
-async onSelectPokemon(pokemon: PokemonBasicInfo) {
-  if (this.selectedPokemons.find(p => p.name === pokemon.name)) return;
-
-  if (this.selectedPokemons.length === 2) {
-    this.selectedPokemons = [];
-}
-const id = this.getIdFromUrl(pokemon.url);
-const details = await firstValueFrom(this.http.get<PokemonDetails>(`https://pokeapi.co/api/v2/pokemon/${id}`));
-this.selectedPokemons.push(details);
-}
-getComparisonResult(): string {
-  const [p1, p2] = this.selectedPokemons;
-  const total1 = this.getTotalStats(p1);
-  const total2 = this.getTotalStats(p2);
-
-  if (total1 > total2) return `${p1.name} é mais forte (${total1} > ${total2})`;
-  if (total2 > total1) return `${p2.name} é mais forte (${total2} > ${total1})`;
-  return `${p1.name} e ${p2.name} têm a mesma força (${total1})`;
-}
-
-getTotalStats(pokemon: PokemonDetails): number {
-  return pokemon.stats.reduce((acc: number, stat: any) => acc + stat.base_stat, 0);
-}
-
-
+  
   @Input() userList: PokemonBasicInfo[] = [];
   @Output() pageChanged = new EventEmitter<PageEvent>();
-
   @Input() limit!: number;
   @Input() totalCount!: number;
   
-
-  displayedColumns: string[] = ['number', 'name', 'details','select'];
-
+  displayedColumns: string[] = ['number', 'name', 'details', 'select'];
   currentPage: number = 0;
+  selectedPokemons: PokemonDetails[] = [];
 
-  onPageChange(event: PageEvent): void {
-    this.pageChanged.emit(event);
-    this.currentPage = event.pageIndex
+  // Verifica se o Pokémon está na lista de selecionados
+  isPokemonSelected(pokemon: PokemonDetails): boolean {
+    return this.selectedPokemons.some(p => p.name === pokemon.name);
+  }
+  
+
+  PokemonSelection(pokemon: PokemonBasicInfo): void {
+
+    const index = this.selectedPokemons.findIndex(p => p.name === pokemon.name);
+
+
+    if (index !== -1) {
+      this.selectedPokemons.splice(index, 1);
+    } 
+ 
+    else if (this.selectedPokemons.length < 2) {
+    
+      this.fetchPokemonDetails(pokemon);
+    }
   }
 
 
+  private async fetchPokemonDetails(pokemon: PokemonBasicInfo) {
+    const id = this.getIdFromUrl(pokemon.url);
+    try {
+      const details = await firstValueFrom(this.http.get<PokemonDetails>(`https://pokeapi.co/api/v2/pokemon/${id}`));
+
+   
+      if (details && details.stats) {
+        this.selectedPokemons.push(details);
+      } else {
+        console.error('Dados incompletos para o Pokémon', pokemon);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar detalhes do Pokémon:', error);
+    }
+  }
+
+
+  getTotalStats(pokemon: PokemonDetails): number {
+    if (!pokemon || !pokemon.stats || !Array.isArray(pokemon.stats)) {
+      console.error('Pokémon sem dados de stats:', pokemon);
+      return 0;
+    }
+    return pokemon.stats.reduce((acc: number, stat: any) => acc + stat.base_stat, 0);
+  }
+
+  getComparisonResult(): string {
+    if (this.selectedPokemons.length < 2) {
+      return 'Selecione dois Pokémons para comparar.';
+    }
+    
+    const [p1, p2] = this.selectedPokemons;
+    const total1 = this.getTotalStats(p1);
+    const total2 = this.getTotalStats(p2);
+
+    if (total1 > total2) return `${p1.name} é mais forte (${total1} > ${total2})`;
+    if (total2 > total1) return `${p2.name} é mais forte (${total2} > ${total1})`;
+    return `${p1.name} e ${p2.name} têm a mesma força (${total1})`;
+  }
+
+
+  onPageChange(event: PageEvent): void {
+    this.pageChanged.emit(event);
+    this.currentPage = event.pageIndex;
+  }
+
+ 
   getIdFromUrl(url: string): string {
     return url.split('/').slice(-2, -1)[0]; 
   }
